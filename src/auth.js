621ab5342e0f76b8a32924a12c165a3bf64018c6
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-04-21 02:29:23 +0800

    Fixed the redirect_uri of authentication

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,3 @@
 import getToken from './spotifyhandler.js'
 
-getToken('3a89b22b7095445782078c237454dafd', 'http://127.0.0.1:8080/', 'user-read-recently-played')
+getToken('3a89b22b7095445782078c237454dafd', 'https://renabil.github.io/recentlyplayedv2', 'user-read-recently-played')
