e155da47f13d77743cd75dca3087f0aa6c513fca
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-04-26 02:06:42 +0800

    v2.2 is now released

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,111 +1,92 @@
 import React from 'react'
 import extractToken from '../extractToken.js'
-import Header from './Header.js'
+import { throws } from 'assert';
 
 class App extends React.Component{
     
     constructor(){
         super();
         this.state = {
-            token: false,
-            data: false,
-            album_pictures: [],
+            token: null,
+            albums: [],
+            recently_played: null,
         }
     }
 
     componentDidMount(){
-
-        // extract the token and then set the state
         this.setState({
-            token: extractToken()
+            token: extractToken(),
         })
-
-        // Set delay as the token will take approx 1ms to set
-        setTimeout(() => {
-            this.getRecentlyPlayed(this.state.token)
-        }, 1);
         
+        setTimeout(() => {
+            this.getRecentlyPlayed(this.state.token, 50)
+        }, 1);
     }
 
-    getRecentlyPlayed(tk){
-
-        console.log(tk)
-        
-        fetch("https://api.spotify.com/v1/me/player/recently-played?limit=50", {
+    getRecentlyPlayed(tk, limit){
+        fetch('https://api.spotify.com/v1/me/player/recently-played?limit=' + limit, {
             headers: {
                 'Authorization': `Bearer ${tk}`
             }
-        })
-        .then(r => {
-            console.log(r.status)
-            if(r.status !== 200 && r.status){
-                this.setState({
-                    data: false
-                })
-            }else{
-                return r.json()
-            }
-
-        })
-        .then(json => {
+        }).then(r => r.json())
+        .then(d => {
+            let items = d.items;
             this.setState({
-                data: json.items
+                recently_played: items
             })
 
-            this.getAlbumPictures(json.items, this.state.token)
-        })
+            items.map(i => {
+                let id = i.track.album.id
 
-    }
-
-    getAlbumPictures(songs, token){
-        songs.map(s => {
-            let id = s.track.album.id
-            fetch(`https://api.spotify.com/v1/albums/${id}`,{
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            })
-            .then(r => r.json())
-            .then(d => {
-                let images_url = d.images[0].url;
-
-                let array = this.state.album_pictures;
-                array.push(images_url)
-                
-                this.setState(p => {
-                    return{
-                        album_pictures: array
-                    }
-                })
-                
+                setTimeout(() => {
+                    this.getAlbums(id, tk)
+                }, 100);
             })
         })
     }
 
+    getAlbums(id, tk){
+        fetch(`https://api.spotify.com/v1/albums/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${tk}`
+            }
+        }).then(r => r.json())
+        .then(d => {
+            let array = this.state.albums;
+            array.push(d)
+            this.setState({
+                albums: array
+            })
+        })
+    }
 
     render(){
 
         return(
-            <div id='body'>   
-                {/* {this.state.data !== false ? this.state.data.map(d => {
-                        return(
-                            <div id='song' >
-                                <h1>{d.track.name}</h1>
-                                <p>By {d.track.artists[0].name}</p>
+            <div>
+                <div id='body'>
+                    {this.state.token !== null ? 
+                        this.state.albums !== [] ? 
+                        this.state.albums.map(d=>{
+                            let img = d.images[0].url;
+                            let albumtype = d.album_type
+                            
+                            // console.log(d)
+                            
+                            return(
+                                <div>
 
-                                <br></br>
-                                
-                                <a id='button' href={`https://open.spotify.com/track/${d.track.id}`}>Spotify</a>
+                                <a href={d.external_urls.spotify}>
+                                    <img id='images' src={img}></img>
+                                </a>
 
-                            </div>
-                        )
-                    }) : null} */}
-
-                    {this.state.data !== false ? this.state.album_pictures.map(i => {
-                        return(
-                            <img onClick={this.handleImageClick} src={i}></img>
-                        )
-                    }) : null}
+                                {/* <p id='album_name' >{d.name}</p> */}
+                                </div>
+                            )
+                        })
+                        : <h1>not loaded</h1>
+                    : <h1>no token</h1>}
+                </div>
             </div>
         )
     }
